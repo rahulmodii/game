40016af97b541c26f4c96eaf479220e4b4a1f04e
rahulmodi <rmodi2407@gmail.com>
2019-04-19 12:26:05 +0530

    buttons

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
+<script>
+
+
+</script>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -15,18 +20,39 @@
                     @endif
 
                    @php
-                   $runs=array('1','2','3','4','6');
-                   $run= array_rand($runs);    
+                   $runs=array('1','4','6','0','9');
+                   $run= array_random($runs);   
                    @endphp
                    <form id="myForm" action="/runs" method="POST">
                        @csrf
-                   <input type="text" name="run" value={{$run}} size= "2" style="border:0" />
-                   <button name="submit" onclick="myFunction()">Click me</button>
-                   </form>
+                   <input type="text" id="fun" name="run" value={{$run}} size= "2" style="border:0" />
+                </form>
+                   <button  id="nuf" value="0" onclick="myFunction(0)">0</button>
+                   <button  id="nuf" value="1" onclick="myFunction(1)">1,2,3</button>
+                   <button  id="nuf" value="4" onclick="myFunction(4)">4</button>
+                   <button  id="nuf" value="6" onclick="myFunction(6)">6</button>
+                   <button  id="nuf" value="0" onclick="myFunction(9)">Out</button>
+                   
                 <div align ="right">your current score {{$score[0]->score}}</div>
                    <script>
-                       function myFunction(){
-                        document.getElementById("myForm").submit();
+                       function myFunction(p){
+                        
+                        var s=document.getElementById("fun").value; 
+                        if (s == p) {
+                            Swal.fire({
+                                        type: 'success',
+                                        title: 'you won',
+                                        })
+                                document.getElementById("myForm").submit();
+                                }                                
+                        else{
+                            Swal.fire({
+                                        type: 'error',
+                                        title: 'you loss',
+                                        })
+                         }                                
+                          
+                        
                        }
 
                    </script>
